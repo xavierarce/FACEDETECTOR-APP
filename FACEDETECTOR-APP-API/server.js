42ccc12141e9de8477938c60399fdeda05dde3f3
xavierarce <119446640+xavierarce@users.noreply.github.com>
2023-07-16 22:19:00 -0500

    main

---
+++ FACEDETECTOR-APP-API/server.js
@@ -0,0 +1,93 @@
+import Express  from "express";
+
+const app = Express();
+
+app.use(Express.json());
+const database = {
+  users:[
+    {
+      id:'123',
+      name : 'John',
+      email: 'john@gmail.com',
+      password:'cookies',
+      entries: 0,
+      joined: new Date()
+    },
+    {
+      id:'124',
+      name : 'Jahn',
+      email: 'jahn@gmail.com',
+      password:'caakies',
+      entries: 0,
+      joined: new Date()
+    }
+  ]
+}
+
+app.get('/',(req,res)=>{
+  res.send(database.users)
+})
+
+app.post('/signin',(req,res)=>{
+  if(req.body.email=== database.users[0].email &&
+    req.body.password === database.users[0].password){
+      res.json('success');
+    }else{
+      res.status(404).json('error logging in')
+    }
+})
+
+app.post('/register',(req,res)=>{
+  const {email, name, password} = req.body;
+  database.users.push({
+    id:'125',
+    name : name,
+    email: email,
+    password: password,
+    entries: 0,
+    joined: new Date()
+  })
+  res.json(database.users[database.users.length-1]);
+})
+
+app.get('/profile/:id',(req,res)=>{
+  const {id}=req.params;
+  let found = false
+  database.users.forEach(user=>{
+    if(user.id === id){
+      found = true;
+      return res.json(user);
+    }
+  })
+  if(!found){
+    res.status(400).json('Not found')
+  }
+})
+
+app.put('/image',(req,res)=>{
+  const {id}=req.body;
+  let found = false
+  database.users.forEach(user=>{
+    if(user.id === id){
+      found = true;
+      user.entries++
+      return res.json(user.entries);
+    }
+  })
+    if(!found){
+    res.status(400).json('Not found')
+  }
+})
+
+app.listen(3000,()=>{
+    console.log('runnin')
+})
+
+
+/* 
+! --> res= this is working
+! signin  --> POST = success/fail
+! register --> POST =  user
+! profile/:userId --> GET = user
+! image --> PUT --> user (count)
+*/ 
