086e49c15dcf4e05a6f0677b3c6ba7325755452b
xavierarce <119446640+xavierarce@users.noreply.github.com>
2023-07-20 15:41:04 -0500

    main

--- FACEDETECTOR-APP-API/server.js
+++ FACEDETECTOR-APP-API/server.js
@@ -1,23 +1,28 @@
 import Express  from "express";
+import bcrypt from "bcryptjs";
+import cors from 'cors';
+
 
 const app = Express();
 
 app.use(Express.json());
+app.use(cors());
+
 const database = {
   users:[
     {
       id:'123',
       name : 'John',
-      email: 'john@gmail.com',
       password:'cookies',
+      email: 'john@gmail.com',
       entries: 0,
       joined: new Date()
     },
     {
       id:'124',
-      name : 'Jahn',
-      email: 'jahn@gmail.com',
-      password:'caakies',
+      name : 'Sally',
+      password:'bananas',
+      email: 'sally@gmail.com',
       entries: 0,
       joined: new Date()
     }
@@ -29,16 +34,31 @@ app.get('/',(req,res)=>{
 })
 
 app.post('/signin',(req,res)=>{
+  //*   // Load hash from your password DB.
+  //* bcrypt.compare("cookies", '$2a$10$wfb8gN0bIZie1pBE/cjE9OSrbK7tCuDuSHGgru/yiua7InbF3aHlu', function(err, res) {
+  //*   // res === true
+  //*   console.log('first guess', res)
+  //* });
+  //* bcrypt.compare("not_bacon", '$2a$10$wfb8gN0bIZie1pBE/cjE9OSrbK7tCuDuSHGgru/yiua7InbF3aHlu', function(err, res) {
+  //*   console.log('second guess', res)
+  //*   // res === false
+  //* });
   if(req.body.email=== database.users[0].email &&
     req.body.password === database.users[0].password){
-      res.json('success');
+      res.json(database.users[0]);
     }else{
+      alert(Error);
       res.status(404).json('error logging in')
     }
 })
 
 app.post('/register',(req,res)=>{
   const {email, name, password} = req.body;
+  //* bcrypt.genSalt(10, function(err, salt) {
+  //*   bcrypt.hash(password, salt, function(err, hash) {
+  //*     console.log(hash)
+  //*   });
+  //* });
   database.users.push({
     id:'125',
     name : name,
@@ -58,7 +78,7 @@ app.get('/profile/:id',(req,res)=>{
       found = true;
       return res.json(user);
     }
-  })
+  })  
   if(!found){
     res.status(400).json('Not found')
   }
@@ -78,6 +98,16 @@ app.put('/image',(req,res)=>{
     res.status(400).json('Not found')
   }
 })
+
+
+
+
+
+// // As of bcryptjs 2.4.0, compare returns a promise if callback is omitted:
+// bcrypt.compare("B4c0/\/", hash).then((res) => {
+//   // res === true
+// });
+
 
 app.listen(3000,()=>{
     console.log('runnin')
